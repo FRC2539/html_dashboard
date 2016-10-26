1b2833c896938b19ba5d022a6468bfd45262d37a
Krypton Cougars <latitude1@kryptoncougars.pasd.us>
2016-10-25 20:14:09 -0400

    Adding Axis camera

--- www/js/index.js
+++ www/js/index.js
@@ -17,17 +17,30 @@ $(document).ready(function() {
     NetworkTables.addRobotConnectionListener(connectionChange, true);
 
     loadCameraOnConnect({
-        container: '#camera1', // where to put the img tag
-        proto: null,                    // optional, defaults to http://
-        host: 'tegra-ubuntu',                     // optional, if null will use robot's autodetected IP address
-        port: 8080,                     // webserver port
-        image_url: '/?action=stream',   // mjpg stream of camera
-        data_url: '/program.json',      // used to test if connection is up
-        wait_img: null,                 // optional img to show when not connected, can use SVG instead
-        error_img: null,                // optional img to show when error connecting, can use SVG instead
-        attrs: {                        // optional: attributes set on svg or img element
-            width: 640,                     // optional, stretches image to this width
-            height: 480,                    // optional, stretches image to this width
+        container: '#camera1',
+        proto: null,
+        host: 'tegra-ubuntu',
+        port: 8080,
+        image_url: '/?action=stream',
+        data_url: '/program.json',
+        wait_img: null,
+        error_img: null,
+        attrs: {
+            width: "100%",
+        }
+        
+    });
+	loadCameraOnConnect({
+        container: '#camera2',
+        proto: null,
+        host: 'axis-camera.local',
+        port: 80,
+        image_url: '/mjpg/video.mjpg',
+        data_url: '/view/index.shtml',
+        wait_img: null,
+        error_img: null,
+        attrs: {
+            width: "100%",
         }
         
     });
