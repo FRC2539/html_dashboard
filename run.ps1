7d26007c4df93584dbdd789f2a3b9a7785734b34
Krypton Cougars <jeff@kryptoncougars.pasd.us>
2016-11-29 21:11:19 -0500

    Adds Powershell script

---
+++ run.ps1
@@ -0,0 +1,5 @@
+﻿cd c:\Users\Krypton\html_dashboard\www
+Start-Process py -ArgumentList "-m pynetworktables2js", "--port 2539", "--dashboard" -NoNewWindow
+cd ..
+npm start
+Stop-Process -processname py
