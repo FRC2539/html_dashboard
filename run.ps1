4287dd7e5e663a6bebb366ee155d3eea323e1920
Krypton Cougars <jeff@kryptoncougars.pasd.us>
2017-01-17 17:55:50 -0500

    Dashboard mode was disabled

--- run.ps1
+++ run.ps1
@@ -1,5 +1,5 @@
 ﻿cd c:\Users\Krypton\html_dashboard\www
-Start-Process py -ArgumentList "-m pynetworktables2js", "--port 2539", "--dashboard" -NoNewWindow
+Start-Process py -ArgumentList "-m pynetworktables2js", "--port 2539", "--robot roborio-2539-frc.local" -NoNewWindow
 cd ..
 npm start
 Stop-Process -processname py
