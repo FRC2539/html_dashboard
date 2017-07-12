cf2a8f34124a3dc8bbddbf72d3e063e1770a12fc
Krypton Cougars <macbook1@kryptoncougars.pasd.us>
2017-07-12 19:13:15 -0400

    More levels of values

--- www/js/debug.js
+++ www/js/debug.js
@@ -1,43 +1,59 @@
 $(document).ready(function($) {
 
+    function addItem($container, id, name) {
+        var li = '<li data-role="collapsible">';
+        $container.append(
+            $(li)
+                .prop('id', id)
+                .append('<h3>' + name + '</h3>')
+        );
+    }
+
     NetworkTables.addGlobalListener(function(key, value, isNew) {
         key = key.split('/');
         key.shift();
 
-        if (isNew)
+        var id = 'nt';
+        var $parent = $('#networktables');
+        for (var i in key)
         {
-            var id = 'nt-';
-            var $parent = $('#networktables');
-            for (var i in key)
+            id += '-' + key[i].replace(/ /g, '_').replace(/~/g, '');
+            if ($('#' + id).length == 0)
             {
-                id += key[i];
-                if ($('#' + id).length == 0)
+                if (i != 0)
                 {
-                    if (i != 0)
+                    var set = $parent.prop('id') + '-set';
+                    if ($('#' + set).length == 0)
                     {
-                        /*$parent.append(
-                            $('<ul>')
-                                .data('role', 'listview')
-                                .prop('id', id)
-                        );*/
-                    }
-                    else
-                    {
-                        var li = '<li data-role="collapsible">';
                         $parent.append(
-                            $(li)
-                                .prop('id', id)
-                                .append('<h3>' + key[i] + '</h3>')
+                            $('<ul data-role="collapsibleset" data-inset="false">')
+                                .prop('id', set)
                         );
                     }
+                    addItem($('#' + set), id, key[i]);
+                }
+                else
+                {
+                    addItem($parent, id, key[i]);
                 }
             }
-            $('#networktables').trigger('create');
+            $parent = $('#' + id);
         }
-        else
+
+        if ( ! Array.isArray(value))
         {
-            $('#nt' + key.join('-')).html(value);
+            if ($('#' + id + '-val').length)
+            {
+                $('#' + id + '-val').html('(' + value + ')');
+            }
+            else
+            {
+                $parent.find('h3').append(
+                    $('<span id="' + id + '-val">(' + value + ')</span>')
+                );
+            }
         }
+        $('#networktables').trigger('create');
 
     }, true);
 
